7a35a83416ce6442835f53a83c48a72f9fce29fc
Sheyla <sheylacortes12@gmail.com>
2026-01-30 10:46:11 +0100

    edito PruebaBanca

--- LaBancaDeDAM/src/Principal/PruebaBanca.java
+++ LaBancaDeDAM/src/Principal/PruebaBanca.java
@@ -1,4 +1,10 @@
 package Principal;
 
+import Metodos.Herramientas;
+
 public class PruebaBanca {
+    public static void main(String[] args) {
+        String opcion;
+        opcion = Herramientas.opcion("Introduzca su usuario ");
+    }
 }
