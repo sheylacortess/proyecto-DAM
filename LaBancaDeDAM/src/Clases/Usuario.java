dec1e9a9b87ff818a8670960e70db939772bc19b
Sheyla <sheylacortes12@gmail.com>
2026-02-06 10:20:44 +0100

    actualizado clase Usuario

--- LaBancaDeDAM/src/Clases/Usuario.java
+++ LaBancaDeDAM/src/Clases/Usuario.java
@@ -1,24 +1,42 @@
 package Clases;
 
 public class Usuario {
+
+    //atributos
     private String nombre;
     private String dni;
     private String email;
 
+    /**
+     * constructor
+     * @param nombre
+     * @param dni
+     * @param email
+     */
     public Usuario(String nombre, String dni, String email) {
         this.nombre = nombre;
         this.dni = dni;
         this.email = email;
     }
+// getters y setters
 
+    /**
+     * @return  nombre del usuario
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @return dni del usuario
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * @return email del usuario
+     */
     public String getEmail() {
         return email;
     }
